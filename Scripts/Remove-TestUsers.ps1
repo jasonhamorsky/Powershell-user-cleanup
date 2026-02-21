c9db4debbc0b096f7dcf13e99e2142b4b66d2293
jasonhamorsky <jason.hamorsky1@outlook.com>
2026-02-21 13:17:06 -0500

    Add files via upload

---
+++ Scripts/Remove-TestUsers.ps1
@@ -0,0 +1,18 @@
+﻿# Cleanup Script: Remove Test Local Users
+# This script deletes the local users created during the bulk user creation lab.
+
+$usersToRemove = @(
+    "awalker",
+    "bjohnson",
+    "csmith"
+)
+
+foreach ($user in $usersToRemove) {
+    try {
+        Remove-LocalUser -Name $user -ErrorAction Stop
+        Write-Host "Removed user: $user"
+    }
+    catch {
+        Write-Host "User $user does not exist or could not be removed."
+    }
+}
